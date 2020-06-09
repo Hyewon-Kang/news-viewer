5adc8b1d4d7f017b01bb29fa5d716cd52411f13d
Hyewon-Kang <gpdnjs22@@>
2020-06-09 19:30:15 +0900

    #14.9

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState, useCallback } from "react";
+import { Route } from "react-router-dom";
+import NewsPage from "./pages/NewsPage";
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos/1").then(response => {
-      setData(response.data);
-    });
-  };
-
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
-    </div>
-  );
+  return <Route path="/:category?" component={NewsPage} />;
 };
 
 export default App;
